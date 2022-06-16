3f9249661c1b369f555e623f4a60c9f7f5b8b271
moraiisjosue <andrejosueplay@gmail.com>
2022-06-15 23:30:14 -0300

    sintaxe básica

---
+++ projects-javascript/sintaxe-basica/aula02 - variaveis/script.js
@@ -0,0 +1,14 @@
+// tipos primitivos
+
+//boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var num = 1;
+console.log(typeof(num));
+
+//String
+
+var nome = 'Josué';
+console.log(typeof(nome));
