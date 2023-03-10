7e5e4c2db2f6a3e58387ef75d7abf5242b0294cf
Josué Morais <andrejosueplay@gmail.com>
2023-03-10 16:28:24 -0300

    alterações

--- projects-javascript/inverter-string/assets/js/script.js
+++ projects-javascript/inverter-string/assets/js/script.js
@@ -1,7 +1,14 @@
-var nome = 'morais';
+/*var nome = 'josue morais';
 var newString = "";
 
 for(var i = nome.length-1; i>=0;i--){
     newString = newString + nome[i];
 }
-console.log(newString);
+console.log(newString);*/
+ 
+const reverseWord = str =>  str.split('').reverse().join('');
+
+console.log(reverseWord('alex lindo'));
+
+
+
